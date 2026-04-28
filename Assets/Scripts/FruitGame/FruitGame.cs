29617d93ddc3f0068ec77b1a8d65fdf228f159f9
Liesame <aaa01062223008@gmail.com>
2026-04-28 12:01:56 +0900

    [Feat] 수박 게임 제작 완료

---
+++ Assets/Scripts/FruitGame/FruitGame.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+
+public class FruitGame : MonoBehaviour
+{
+
+    public GameObject[] fruitPrefabs;
+    public float[] fruitSizes = { 0.5f, 0.7f, 0.9f, 1.1f, 1.3f, 1.5f, 1.7f, 1.9f };
+
+    public GameObject currentFruit;
+    public int currentFruitType;
+
+    public float fruitStarHeight = 6.0f;
+    public float gameWidth = 5.0f;
+    public bool isGameOver = false;
+    public Camera mainCamear;
+
+    public float fruitTimer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamear = Camera.main;
+        SpawnNewFruit();
+        fruitTimer = -3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOver) return;
+
+        if (fruitTimer >= 0)
+        {
+            fruitTimer -= Time.deltaTime;
+        }
+
+        if (fruitTimer < 0 && fruitTimer > -2)
+        {
+            SpawnNewFruit();
+            fruitTimer = -3.0f;
+        }
+
+        if (currentFruit != null)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 worldPosition = mainCamear.ScreenToWorldPoint(mousePosition);
+
+            Vector3 newPoisition = new Vector3(worldPosition.x, fruitStarHeight, 0);
+            newPoisition.x = worldPosition.x;
+
+            float halfFruitSize = fruitSizes[currentFruitType] / 2f;
+
+            if (newPoisition.x < -gameWidth / 2 + halfFruitSize)
+            {
+                newPoisition.x = -gameWidth / 2 + halfFruitSize;
+            }
+
+            if (newPoisition.x > gameWidth / 2 + halfFruitSize)
+            {
+                newPoisition.x = gameWidth / 2 + halfFruitSize;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && fruitTimer == -3.0f)
+        {
+            DropFruit();
+        }
+    }
+
+    void SpawnNewFruit()
+    {
+        if (!isGameOver)
+        {
+            currentFruitType = Random.Range(0, 3);
+
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 worldPosition = mainCamear.ScreenToWorldPoint(mousePosition);
+
+            Vector3 spawnPosition = new Vector3(worldPosition.x, fruitStarHeight, 0);
+
+            float halfFruitSize = fruitSizes[currentFruitType] / 2f;
+
+            spawnPosition.x = Mathf.Clamp(spawnPosition.x, -gameWidth / 2 + halfFruitSize, gameWidth / 2 - halfFruitSize);
+
+            currentFruit = Instantiate(fruitPrefabs[currentFruitType], spawnPosition, Quaternion.identity);
+            currentFruit.transform.localScale = new Vector3(fruitSizes[currentFruitType], fruitSizes[currentFruitType], 1);
+
+            Rigidbody2D rb = currentFruit.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                rb.gravityScale = 0f;
+            }
+        }
+    }
+
+    void DropFruit()
+    {
+        Rigidbody2D rb = currentFruit.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.gravityScale = 1f;
+            currentFruit = null;
+            fruitTimer = 1.0f;
+        }
+    }
+
+    public void MergeFruits(int fruitType, Vector3 positioin)
+    {
+        if (fruitType < fruitPrefabs.Length - 1)
+        {
+            GameObject newFruit = Instantiate(fruitPrefabs[fruitType + 1], positioin, Quaternion.identity);
+            newFruit.transform.localScale = new Vector3(fruitSizes[fruitType + 1], fruitSizes[fruitType + 1], 1.0f);
+        }
+    }
+}
